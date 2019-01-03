0677d0ef2c14513490ccbaba63066098b40ce16d
Nasirou Wagana <wnasirou@gmail.com>
2019-01-03 11:02:21 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <component :is="component">
+    <slot />
+  </component>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import Default from './layouts/Default'
+// import Login from './layouts/Login'
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: 'App',
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  components: {
+    Default
+    // Login
+  },
+
+  computed: {
+    component () {
+      // if (this.$route.path === '/login') {
+      //   return Login
+      // }
+
+      return Default
+    }
+  }
 }
-</style>
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/Home')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/About')
+    }
+  ]
+})
